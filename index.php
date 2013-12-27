46919151997ec6d8e6348ede6ed9397cffac6ff0
Menesty <ahnezdyuk@gmail.com>
2013-12-27 19:01:28 +0200

    Router

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
-include_once ("org/menesty/server/DigestAuthentication.php");
+include_once("org/menesty/server/DigestAuthentication.php");
 
 $digestAuthentication = new DigestAuthentication();
 $digestAuthentication->auth();
 
 
+echo dirname(__FILE__);
+
+//http://habrahabr.ru/post/31270/
+//http://www.php.net/manual/en/function.error-log.php
+
+class Router
+{
+
+    private $controllerPath;
+
+    public function setPath($path){
+
+    }
+
+
+}
 
 ?>
---
+++ org/menesty/server/controller/IndexController.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * User: Menesty
+ * Date: 12/27/13
+ * Time: 6:52 PM
+ */ 
